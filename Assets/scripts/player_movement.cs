1d7e599841a0d5ae000238717c53a995f35b4410
knebalazs <knebalazs@yahoo.com>
2021-10-21 12:20:02 +0200

    added lights, lamp

--- Assets/scripts/player_movement.cs
+++ Assets/scripts/player_movement.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour{
     public float speed = 2f;
+    float offset;
     public Animator anim;
     private bool is_rowing = false;
     private SpriteRenderer sp_renderer;
-    
+
 
     // Start is called before the first frame update
     void Start(){
-        sp_renderer = GetComponent<SpriteRenderer>();   
+        sp_renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -33,9 +34,11 @@ public class player_movement : MonoBehaviour{
         }
         if(Input.GetButton("Jump"))
         {
-            Vector3 new_pos = GameObject.FindGameObjectWithTag("boat").transform.position + new Vector3(0, 0, 0);
-            this.transform.position = new_pos;
-            GameObject.FindGameObjectWithTag("background").transform.Translate(-1 * Time.deltaTime, 0, 0);
+            //Vector3 new_pos = GameObject.FindGameObjectWithTag("boat").transform.position + new Vector3(0, 0, 0);
+            //this.transform.position = new_pos;
+            // GameObject.FindGameObjectWithTag("background").transform.Translate(-1 * Time.deltaTime, 0, 0);
+            offset += 0.2f * Time.deltaTime / 10f;
+            GameObject.FindGameObjectWithTag("background").GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
         }
         if (Input.GetButtonUp("Jump"))
         {
@@ -43,6 +46,11 @@ public class player_movement : MonoBehaviour{
             anim.SetBool("Is_row", is_rowing);
             this.sp_renderer.sortingOrder = 1;
         }
+
+        if (sp_renderer.flipX)
+            GameObject.FindGameObjectWithTag("lamp").transform.position = this.transform.position + new Vector3(-1f, 0.05f, 0);
+        else
+            GameObject.FindGameObjectWithTag("lamp").transform.position = this.transform.position + new Vector3(1f, 0.05f, 0);
 
 
         if (GameObject.FindGameObjectWithTag("fishing").GetComponent<fishing>().is_casting)
